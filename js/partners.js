9a50f088f9dc3f9d14b57fd5fba5e60785dc1421
Danielzhangsl <2249099670@qq.com>
2022-09-06 20:27:32 +0800

    Paretners
    合作伙伴页面

---
+++ js/partners.js
@@ -0,0 +1,561 @@
+var windows = document.getElementsByClassName("windows")[0]
+var windows_one = document.getElementsByClassName("windows_one")[0]
+var windows_two = document.getElementsByClassName("windows_two")[0]
+var windows_there = document.getElementsByClassName("windows_there")[0]
+var windows_four = document.getElementsByClassName("windows_four")[0]
+var windows_five = document.getElementsByClassName("windows_five")[0]
+var windows_six = document.getElementsByClassName("windows_six")[0]
+var windows_seven = document.getElementsByClassName("windows_seven")[0]
+var partner_one = document.getElementsByClassName("partner_one")[0]
+var partner_two = document.getElementsByClassName("partner_two")[0]
+var partner_there = document.getElementsByClassName("partner_there")[0]
+var partner_four = document.getElementsByClassName("partner_four")[0]
+var partner_five = document.getElementsByClassName("partner_five")[0]
+var partner_six = document.getElementsByClassName("partner_six")[0]
+var partner_seven = document.getElementsByClassName("partner_seven")[0]
+var partner_eight = document.getElementsByClassName("partner_eight")[0]
+
+function ones() {
+   
+
+   partner_one.style.background = "#ecf0f5"
+   partner_there.style.background = "#f7f6f7"
+   partner_four.style.background = "#f7f6f7"
+   partner_five.style.background = "#f7f6f7"
+   partner_six.style.background = "#f7f6f7"
+   partner_seven.style.background = "#f7f6f7"
+   partner_eight.style.background = "#f7f6f7"
+   partner_two.style.background = "#f7f6f7"
+   partner_two.onmouseover = function () {
+      partner_two.style.background = "#ecf0f5"
+   }
+   partner_two.onmouseleave = function () {
+      partner_two.style.background = "#f7f6f7"
+   }
+   partner_there.onmouseover = function () {
+      partner_there.style.background = "#ecf0f5"
+   }
+   partner_there.onmouseleave = function () {
+      partner_there.style.background = "#f7f6f7"
+   }
+   partner_four.onmouseover = function () {
+      partner_four.style.background = "#ecf0f5"
+   }
+   partner_four.onmouseleave = function () {
+      partner_four.style.background = "#f7f6f7"
+   }
+   partner_five.onmouseover = function () {
+      partner_five.style.background = "#ecf0f5"
+   }
+   partner_five.onmouseleave = function () {
+      partner_five.style.background = "#f7f6f7"
+   }
+   partner_six.onmouseover = function () {
+      partner_six.style.background = "#ecf0f5"
+   }
+   partner_six.onmouseleave = function () {
+      partner_six.style.background = "#f7f6f7"
+   }
+   partner_seven.onmouseover = function () {
+      partner_seven.style.background = "#ecf0f5"
+   }
+   partner_seven.onmouseleave = function () {
+      partner_seven.style.background = "#f7f6f7"
+   }
+   partner_eight.onmouseover = function () {
+      partner_eight.style.background = "#ecf0f5"
+   }
+   partner_eight.onmouseleave = function () {
+      partner_eight.style.background = "#f7f6f7"
+   }
+   windows.style.display = "block"
+   windows_one.style.display = "none"
+   windows_two.style.display = "none"
+   windows_there.style.display = "none"
+   windows_four.style.display = "none"
+   windows_five.style.display = "none"
+   windows_six.style.display = "none"
+   windows_seven.style.display = "none"
+}
+function twos() {
+   
+
+   partner_one.style.background = "#f7f6f7"
+   partner_there.style.background = "#f7f6f7"
+   partner_four.style.background = "#f7f6f7"
+   partner_five.style.background = "#f7f6f7"
+   partner_six.style.background = "#f7f6f7"
+   partner_seven.style.background = "#f7f6f7"
+   partner_eight.style.background = "#f7f6f7"
+   partner_two.style.background = "#ecf0f5"
+
+   partner_one.onmouseover = function () {
+      partner_one.style.background = "#ecf0f5"
+   }
+   partner_one.onmouseleave = function () {
+      partner_one.style.background = "#f7f6f7"
+   }
+   partner_there.onmouseover = function () {
+      partner_there.style.background = "#ecf0f5"
+   }
+   partner_there.onmouseleave = function () {
+      partner_there.style.background = "#f7f6f7"
+   }
+   partner_four.onmouseover = function () {
+      partner_four.style.background = "#ecf0f5"
+   }
+   partner_four.onmouseleave = function () {
+      partner_four.style.background = "#f7f6f7"
+   }
+   partner_five.onmouseover = function () {
+      partner_five.style.background = "#ecf0f5"
+   }
+   partner_five.onmouseleave = function () {
+      partner_five.style.background = "#f7f6f7"
+   }
+   partner_six.onmouseover = function () {
+      partner_six.style.background = "#ecf0f5"
+   }
+   partner_six.onmouseleave = function () {
+      partner_six.style.background = "#f7f6f7"
+   }
+   partner_seven.onmouseover = function () {
+      partner_seven.style.background = "#ecf0f5"
+   }
+   partner_seven.onmouseleave = function () {
+      partner_seven.style.background = "#f7f6f7"
+   }
+   partner_eight.onmouseover = function () {
+      partner_eight.style.background = "#ecf0f5"
+   }
+   partner_eight.onmouseleave = function () {
+      partner_eight.style.background = "#f7f6f7"
+   }
+   windows.style.display = "none"
+   windows_one.style.display = "block"
+   windows_two.style.display = "none"
+   windows_there.style.display = "none"
+   windows_four.style.display = "none"
+   windows_five.style.display = "none"
+   windows_six.style.display = "none"
+   windows_seven.style.display = "none"
+}
+
+function threes() {
+  
+
+   partner_one.style.background = "#f7f6f7"
+   partner_there.style.background = "#ecf0f5"
+   partner_four.style.background = "#f7f6f7"
+   partner_five.style.background = "#f7f6f7"
+   partner_six.style.background = "#f7f6f7"
+   partner_seven.style.background = "#f7f6f7"
+   partner_eight.style.background = "#f7f6f7"
+   partner_two.style.background = "#f7f6f7"
+   partner_one.onmouseover = function () {
+      partner_one.style.background = "#ecf0f5"
+   }
+   partner_one.onmouseleave = function () {
+      partner_one.style.background = "#f7f6f7"
+   }
+   partner_two.onmouseover = function () {
+      partner_two.style.background = "#ecf0f5"
+   }
+   partner_two.onmouseleave = function () {
+      partner_two.style.background = "#f7f6f7"
+   }
+   partner_four.onmouseover = function () {
+      partner_four.style.background = "#ecf0f5"
+   }
+   partner_four.onmouseleave = function () {
+      partner_four.style.background = "#f7f6f7"
+   }
+   partner_five.onmouseover = function () {
+      partner_five.style.background = "#ecf0f5"
+   }
+   partner_five.onmouseleave = function () {
+      partner_five.style.background = "#f7f6f7"
+   }
+   partner_six.onmouseover = function () {
+      partner_six.style.background = "#ecf0f5"
+   }
+   partner_six.onmouseleave = function () {
+      partner_six.style.background = "#f7f6f7"
+   }
+   partner_seven.onmouseover = function () {
+      partner_seven.style.background = "#ecf0f5"
+   }
+   partner_seven.onmouseleave = function () {
+      partner_seven.style.background = "#f7f6f7"
+   }
+   partner_eight.onmouseover = function () {
+      partner_eight.style.background = "#ecf0f5"
+   }
+   partner_eight.onmouseleave = function () {
+      partner_eight.style.background = "#f7f6f7"
+   }
+   windows.style.display = "none"
+   windows_one.style.display = "none"
+   windows_two.style.display = "block"
+   windows_there.style.display = "none"
+   windows_four.style.display = "none"
+   windows_five.style.display = "none"
+   windows_six.style.display = "none"
+   windows_seven.style.display = "none"
+}
+function fours() {
+  
+
+   partner_one.style.background = "#f7f6f7"
+   partner_there.style.background = "#f7f6f7"
+   partner_four.style.background = "#ecf0f5"
+   partner_five.style.background = "#f7f6f7"
+   partner_six.style.background = "#f7f6f7"
+   partner_seven.style.background = "#f7f6f7"
+   partner_eight.style.background = "#f7f6f7"
+   partner_two.style.background = "#f7f6f7"
+   partner_one.onmouseover = function () {
+      partner_one.style.background = "#ecf0f5"
+   }
+   partner_one.onmouseleave = function () {
+      partner_one.style.background = "#f7f6f7"
+   }
+   partner_there.onmouseover = function () {
+      partner_there.style.background = "#ecf0f5"
+   }
+   partner_there.onmouseleave = function () {
+      partner_there.style.background = "#f7f6f7"
+   }
+   partner_two.onmouseover = function () {
+      partner_two.style.background = "#ecf0f5"
+   }
+   partner_two.onmouseleave = function () {
+      partner_two.style.background = "#f7f6f7"
+   }
+   partner_five.onmouseover = function () {
+      partner_five.style.background = "#ecf0f5"
+   }
+   partner_five.onmouseleave = function () {
+      partner_five.style.background = "#f7f6f7"
+   }
+   partner_six.onmouseover = function () {
+      partner_six.style.background = "#ecf0f5"
+   }
+   partner_six.onmouseleave = function () {
+      partner_six.style.background = "#f7f6f7"
+   }
+   partner_seven.onmouseover = function () {
+      partner_seven.style.background = "#ecf0f5"
+   }
+   partner_seven.onmouseleave = function () {
+      partner_seven.style.background = "#f7f6f7"
+   }
+   partner_eight.onmouseover = function () {
+      partner_eight.style.background = "#ecf0f5"
+   }
+   partner_eight.onmouseleave = function () {
+      partner_eight.style.background = "#f7f6f7"
+   }
+   windows.style.display = "none"
+   windows_one.style.display = "none"
+   windows_two.style.display = "none"
+   windows_there.style.display = "block"
+   windows_four.style.display = "none"
+   windows_five.style.display = "none"
+   windows_six.style.display = "none"
+   windows_seven.style.display = "none"
+}
+function fives() {
+  
+
+   partner_one.style.background = "#f7f6f7"
+   partner_there.style.background = "#f7f6f7"
+   partner_four.style.background = "#f7f6f7"
+   partner_five.style.background = "#ecf0f5"
+   partner_six.style.background = "#f7f6f7"
+   partner_seven.style.background = "#f7f6f7"
+   partner_eight.style.background = "#f7f6f7"
+   partner_two.style.background = "#f7f6f7"
+
+   partner_one.onmouseover = function () {
+      partner_one.style.background = "#ecf0f5"
+   }
+   partner_one.onmouseleave = function () {
+      partner_one.style.background = "#f7f6f7"
+   }
+   partner_there.onmouseover = function () {
+      partner_there.style.background = "#ecf0f5"
+   }
+   partner_there.onmouseleave = function () {
+      partner_there.style.background = "#f7f6f7"
+   }
+   partner_four.onmouseover = function () {
+      partner_four.style.background = "#ecf0f5"
+   }
+   partner_four.onmouseleave = function () {
+      partner_four.style.background = "#f7f6f7"
+   }
+   partner_two.onmouseover = function () {
+      partner_two.style.background = "#ecf0f5"
+   }
+   partner_two.onmouseleave = function () {
+      partner_two.style.background = "#f7f6f7"
+   }
+   partner_six.onmouseover = function () {
+      partner_six.style.background = "#ecf0f5"
+   }
+   partner_six.onmouseleave = function () {
+      partner_six.style.background = "#f7f6f7"
+   }
+   partner_seven.onmouseover = function () {
+      partner_seven.style.background = "#ecf0f5"
+   }
+   partner_seven.onmouseleave = function () {
+      partner_seven.style.background = "#f7f6f7"
+   }
+   partner_eight.onmouseover = function () {
+      partner_eight.style.background = "#ecf0f5"
+   }
+   partner_eight.onmouseleave = function () {
+      partner_eight.style.background = "#f7f6f7"
+   }
+   windows.style.display = "none"
+   windows_one.style.display = "none"
+   windows_two.style.display = "none"
+   windows_there.style.display = "none"
+   windows_four.style.display = "block"
+   windows_five.style.display = "none"
+   windows_six.style.display = "none"
+   windows_seven.style.display = "none"
+}
+function sixs() {
+   
+
+   partner_one.style.background = "#f7f6f7"
+   partner_there.style.background = "#f7f6f7"
+   partner_four.style.background = "#f7f6f7"
+   partner_five.style.background = "#f7f6f7"
+   partner_six.style.background = "#ecf0f5"
+   partner_seven.style.background = "#f7f6f7"
+   partner_eight.style.background = "#f7f6f7"
+   partner_two.style.background = "#f7f6f7"
+   partner_one.onmouseover = function () {
+      partner_one.style.background = "#ecf0f5"
+   }
+   partner_one.onmouseleave = function () {
+      partner_one.style.background = "#f7f6f7"
+   }
+   partner_there.onmouseover = function () {
+      partner_there.style.background = "#ecf0f5"
+   }
+   partner_there.onmouseleave = function () {
+      partner_there.style.background = "#f7f6f7"
+   }
+   partner_four.onmouseover = function () {
+      partner_four.style.background = "#ecf0f5"
+   }
+   partner_four.onmouseleave = function () {
+      partner_four.style.background = "#f7f6f7"
+   }
+   partner_five.onmouseover = function () {
+      partner_five.style.background = "#ecf0f5"
+   }
+   partner_five.onmouseleave = function () {
+      partner_five.style.background = "#f7f6f7"
+   }
+   partner_two.onmouseover = function () {
+      partner_two.style.background = "#ecf0f5"
+   }
+   partner_two.onmouseleave = function () {
+      partner_two.style.background = "#f7f6f7"
+   }
+   partner_seven.onmouseover = function () {
+      partner_seven.style.background = "#ecf0f5"
+   }
+   partner_seven.onmouseleave = function () {
+      partner_seven.style.background = "#f7f6f7"
+   }
+   partner_eight.onmouseover = function () {
+      partner_eight.style.background = "#ecf0f5"
+   }
+   partner_eight.onmouseleave = function () {
+      partner_eight.style.background = "#f7f6f7"
+   }
+   windows.style.display = "none"
+   windows_one.style.display = "none"
+   windows_two.style.display = "none"
+   windows_there.style.display = "none"
+   windows_four.style.display = "none"
+   windows_five.style.display = "block"
+   windows_six.style.display = "none"
+   windows_seven.style.display = "none"
+}
+function sevens() {
+   
+
+   partner_one.style.background = "#f7f6f7"
+   partner_there.style.background = "#f7f6f7"
+   partner_four.style.background = "#f7f6f7"
+   partner_five.style.background = "#f7f6f7"
+   partner_six.style.background = "#f7f6f7"
+   partner_seven.style.background = "#ecf0f5"
+   partner_eight.style.background = "#f7f6f7"
+   partner_two.style.background = "#f7f6f7"
+
+   partner_one.onmouseover = function () {
+      partner_one.style.background = "#ecf0f5"
+   }
+   partner_one.onmouseleave = function () {
+      partner_one.style.background = "#f7f6f7"
+   }
+   partner_there.onmouseover = function () {
+      partner_there.style.background = "#ecf0f5"
+   }
+   partner_there.onmouseleave = function () {
+      partner_there.style.background = "#f7f6f7"
+   }
+   partner_four.onmouseover = function () {
+      partner_four.style.background = "#ecf0f5"
+   }
+   partner_four.onmouseleave = function () {
+      partner_four.style.background = "#f7f6f7"
+   }
+   partner_five.onmouseover = function () {
+      partner_five.style.background = "#ecf0f5"
+   }
+   partner_five.onmouseleave = function () {
+      partner_five.style.background = "#f7f6f7"
+   }
+   partner_six.onmouseover = function () {
+      partner_six.style.background = "#ecf0f5"
+   }
+   partner_six.onmouseleave = function () {
+      partner_six.style.background = "#f7f6f7"
+   }
+   partner_two.onmouseover = function () {
+      partner_two.style.background = "#ecf0f5"
+   }
+   partner_two.onmouseleave = function () {
+      partner_two.style.background = "#f7f6f7"
+   }
+   partner_eight.onmouseover = function () {
+      partner_eight.style.background = "#ecf0f5"
+   }
+   partner_eight.onmouseleave = function () {
+      partner_eight.style.background = "#f7f6f7"
+   }
+   windows.style.display = "none"
+   windows_one.style.display = "none"
+   windows_two.style.display = "none"
+   windows_there.style.display = "none"
+   windows_four.style.display = "none"
+   windows_five.style.display = "none"
+   windows_six.style.display = "block"
+   windows_seven.style.display = "none"
+}
+function eights() {
+   
+
+   partner_one.style.background = "#f7f6f7"
+   partner_there.style.background = "#f7f6f7"
+   partner_four.style.background = "#f7f6f7"
+   partner_five.style.background = "#f7f6f7"
+   partner_six.style.background = "#f7f6f7"
+   partner_seven.style.background = "#f7f6f7"
+   partner_eight.style.background = "#ecf0f5"
+   partner_two.style.background = "#f7f6f7"
+   partner_one.onmouseover = function () {
+      partner_one.style.background = "#ecf0f5"
+   }
+   partner_one.onmouseleave = function () {
+      partner_one.style.background = "#f7f6f7"
+   }
+   partner_there.onmouseover = function () {
+      partner_there.style.background = "#ecf0f5"
+   }
+   partner_there.onmouseleave = function () {
+      partner_there.style.background = "#f7f6f7"
+   }
+   partner_four.onmouseover = function () {
+      partner_four.style.background = "#ecf0f5"
+   }
+   partner_four.onmouseleave = function () {
+      partner_four.style.background = "#f7f6f7"
+   }
+   partner_five.onmouseover = function () {
+      partner_five.style.background = "#ecf0f5"
+   }
+   partner_five.onmouseleave = function () {
+      partner_five.style.background = "#f7f6f7"
+   }
+   partner_six.onmouseover = function () {
+      partner_six.style.background = "#ecf0f5"
+   }
+   partner_six.onmouseleave = function () {
+      partner_six.style.background = "#f7f6f7"
+   }
+   partner_seven.onmouseover = function () {
+      partner_seven.style.background = "#ecf0f5"
+   }
+   partner_seven.onmouseleave = function () {
+      partner_seven.style.background = "#f7f6f7"
+   }
+   partner_two.onmouseover = function () {
+      partner_two.style.background = "#ecf0f5"
+   }
+   partner_two.onmouseleave = function () {
+      partner_two.style.background = "#f7f6f7"
+   }
+   windows.style.display = "none"
+   windows_one.style.display = "none"
+   windows_two.style.display = "none"
+   windows_there.style.display = "none"
+   windows_four.style.display = "none"
+   windows_five.style.display = "none"
+   windows_six.style.display = "none"
+   windows_seven.style.display = "block"
+}
+
+
+      //   function two(){
+      //    partner_two.style.background="#ecf0f5"
+      //   }
+      //   function twoss(){
+      //    partner_two.style.background="#f7f6f7"
+      //   }
+      //   function three(){
+      //    partner_there.style.background="#ecf0f5"
+      //   }
+      //   function threess(){
+      //    partner_there.style.background="#f7f6f7"
+      //   }
+      //   function four(){
+      //    partner_four.style.background="#ecf0f5"
+      //   }
+      //   function fourss(){
+      //    partner_four.style.background="#f7f6f7"
+      //   }
+      //   function five(){
+      //    partner_five.style.background="#ecf0f5"
+      //   }
+      //   function fivess(){
+      //    partner_five.style.background="#f7f6f7"
+      //   }
+      //   function six(){
+      //    partner_six.style.background="#ecf0f5"
+      //   }
+      //   function sixss(){
+      //    partner_six.style.background="#f7f6f7"
+      //   }
+      //   function seven(){
+      //    partner_seven.style.background="#ecf0f5"
+      //   }
+      //   function sevenss(){
+      //    partner_seven.style.background="#f7f6f7"
+      //   }
+      //   function eight(){
+      //    partner_eight.style.background="#ecf0f5"
+      //   }
+      //   function eightss(){
+      //    partner_eight.style.background="#f7f6f7"
+      //   }
